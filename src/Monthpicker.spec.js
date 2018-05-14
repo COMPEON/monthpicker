6553494516555bc17a7c7ba1a07d51966a26861e
Bastian Ahrens <bahrens@compeon.de>
2018-05-14 20:52:57 +0200

    Add first specs for monthpicker

---
+++ src/Monthpicker.spec.js
@@ -0,0 +1,112 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import Monthpicker from '.'
+
+describe('<Monthpicker />', () => {
+  it('renders its children', () => {
+    const wrapper = shallow(
+      <Monthpicker month={1} year={2010} onChange={jest.fn()}>Child</Monthpicker>
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  describe('event handling: ', () => {
+
+    it('opens the overlay when clicking on the child', () => {
+      const wrapper = mount(
+        <Monthpicker initialYear={2010} month={1} year={2010} onChange={jest.fn()}>
+          <div className='child'>Child</div>
+        </Monthpicker>
+      )
+
+      const child = wrapper.find('.child').first()
+
+      child.simulate('click')
+
+      expect(wrapper).toMatchSnapshot()
+    })
+
+    it('closes the overlay when clicking anywhere outside the overlay', () => {
+      const wrapper = mount(
+        <Monthpicker initialYear={2010} month={1} year={2010} onChange={jest.fn()}>
+          <div className='child'>Child</div>
+        </Monthpicker>
+      )
+
+      const child = wrapper.find('.child').first()
+
+      child.simulate('click')
+      child.simulate('click')
+
+      expect(wrapper).toMatchSnapshot()
+    })
+
+    it('sets the focussed date to 1 month later when hitting ArrowRight', () => {
+      const initialyFocussedMonth = 1
+      const wrapper = mount(
+        <Monthpicker initialYear={2010} month={initialyFocussedMonth} year={2010} onChange={jest.fn()}>
+          <div className='child'>Child</div>
+        </Monthpicker>
+      )
+
+      const child = wrapper.find('.child').first()
+      child.simulate('click')
+
+      wrapper.simulate('keyDown', { key: 'ArrowRight' })
+
+      const focussedMonth = wrapper.findWhere(node => node.key() === 'Februar')
+      expect(focussedMonth).toMatchSnapshot()
+    })
+
+    it('sets the focussed date to 1 month earlier when hitting ArrowLeft', () => {
+      const initialyFocussedMonth = 2
+      const wrapper = mount(
+        <Monthpicker initialYear={2010} month={initialyFocussedMonth} year={2010} onChange={jest.fn()}>
+          <div className='child'>Child</div>
+        </Monthpicker>
+      )
+
+      const child = wrapper.find('.child').first()
+      child.simulate('click')
+
+      wrapper.simulate('keyDown', { key: 'ArrowLeft' })
+
+      const focussedMonth = wrapper.findWhere(node => node.key() === 'Januar')
+      expect(focussedMonth).toMatchSnapshot()
+    })
+
+    it('sets the focussed date to 3 month later when hitting ArrowDown', () => {
+      const initialyFocussedMonth = 1
+      const wrapper = mount(
+        <Monthpicker initialYear={2010} month={initialyFocussedMonth} year={2010} onChange={jest.fn()}>
+          <div className='child'>Child</div>
+        </Monthpicker>
+      )
+
+      const child = wrapper.find('.child').first()
+      child.simulate('click')
+
+      wrapper.simulate('keyDown', { key: 'ArrowDown' })
+
+      const focussedMonth = wrapper.findWhere(node => node.key() === 'April')
+      expect(focussedMonth).toMatchSnapshot()
+    })
+
+    it('sets the focussed date to 3 month earlier when hitting ArrowUp', () => {
+      const initialyFocussedMonth = 4
+      const wrapper = mount(
+        <Monthpicker initialYear={2010} month={initialyFocussedMonth} year={2010} onChange={jest.fn()}>
+          <div className='child'>Child</div>
+        </Monthpicker>
+      )
+
+      const child = wrapper.find('.child').first()
+      child.simulate('click')
+
+      wrapper.simulate('keyDown', { key: 'ArrowUp' })
+
+      const focussedMonth = wrapper.findWhere(node => node.key() === 'Januar')
+      expect(focussedMonth).toMatchSnapshot()
+    })
+  })
+})
