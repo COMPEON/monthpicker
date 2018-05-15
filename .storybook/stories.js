394dd0dea503320b358d13ffed6769687065f94c
Bastian Ahrens <bahrens@compeon.de>
2018-05-15 10:14:05 +0200

    Adjust monthpicker stories

--- .storybook/stories.js
+++ .storybook/stories.js
@@ -3,7 +3,25 @@ import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import Monthpicker from '../src'
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Monthpicker onChange={console.log} month={1} year={2018}>Test</Monthpicker>
+storiesOf('Monthpicker', module)
+  .add('without an initial date', () => (
+    <Monthpicker onChange={console.log}>Test</Monthpicker>
+  ))
+  .add('with an initial date', () => (
+    <Monthpicker month={2} year={2018} onChange={console.log}>Test</Monthpicker>
+  ))
+  .add('with an initial year set', () => (
+    <Monthpicker initialYear={2017} month={2} year={2018} onChange={console.log}>Test</Monthpicker>
+  ))
+  .add('with different colors', () => (
+    <Monthpicker
+      month={5}
+      year={2018}
+      onChange={console.log}
+      primaryColor='#fab400'
+      secondaryColor='#550000'
+      hoverColor='red'
+    >
+      Test
+    </Monthpicker>
   ))
